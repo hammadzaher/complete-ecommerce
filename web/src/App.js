18e243531a5df28501304116d7a1fa6f10f119e4
HAMMAD <mac@macs-MacBook-Pro.local>
2025-07-17 11:01:26 -0700

    profile

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Router from './component/Router.jsx'
+import api from './component/Api.js'
+import { useContext, useEffect } from 'react';
+import { GlobalContext } from './Caontext/Context.js'
 
 function App() {
+  let { state, dispatch } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        let res = await api.get('/profile');
+        dispatch({ type: "USER_LOGIN", user: res.data?.user })
+
+      } catch (error) {
+        dispatch({ type: "USER_LOGOUT" })
+      }
+    }
+    getUserData();
+  }, [])
   return (
     <Router />
   );
